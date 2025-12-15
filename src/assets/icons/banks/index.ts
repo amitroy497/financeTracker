43303ce19f487c727132262378a9f2a1d16a266d
Amit Roy <amitroy497@yahoo.com>
2025-12-15 22:11:14 +0530

    chore: update

--- src/assets/icons/banks/index.ts
+++ src/assets/icons/banks/index.ts
@@ -1,5 +1,24 @@
+import abBank from './ab-bank-icon.png';
+import agvBank from './agvb-icon.png';
+import airtelPaymentsBank from './airtel-icon.png';
+import amexCorporation from './amex-icon.png';
+import anzBank from './anzb-icon.png';
+import apgBank from './apgb-icon.png';
+import aprBank from './aprb-icon.png';
 import auSmallFinanceBank from './au-icon.png';
 import axisBank from './axis-icon.png';
+import bandhanBank from './bandhanBank-icon.png';
+import bankOfCeylon from './bankOfCeylon-icon.png';
+import bankOfChina from './bankOfChina-icon.png';
+import bankOfIndia from './bankOfIndia-icon.png';
+import bankOfMaharashtra from './bankOfMaharashtra-icon.png';
+import bankOfNovaScotia from './bankOfNovaScotia-icon.png';
+import barclays from './barclays-icon.png';
+import biharGraminBank from './biharGraminBank-icon.png';
+import bnpParibas from './bnpParibas-icon.png';
+import boa from './boa-icon.png';
+import bankOfBaroda from './bob-icon.png';
+import bankOfBahrainKuwait from './bobkbsc-icon.png';
 import defaultBank from './default-bank-icon.png';
 import federalBank from './federal-icon.png';
 import hdfcBank from './hdfc-icon.png';
@@ -10,19 +29,136 @@ import kotakMahindraBank from './kotak-icon.png';
 import punjabNationalBank from './pnb-icon.png';
 import sbiBank from './sbi-icon.png';
 import ucoBank from './uco-icon.png';
-// Export all bank icons
+
 export const bankIcons = {
-	'Axis Bank': axisBank,
-	'AU Small Finance Bank': auSmallFinanceBank,
-	'Federal Bank': federalBank,
-	'HDFC Bank': hdfcBank,
-	'ICICI Bank': iciciBank,
-	'IDFC First Bank': idfcFirstBank,
-	'Kotak Mahindra Bank': kotakMahindraBank,
+	'Axis Bank Ltd.': axisBank,
+	'Au Small Finance Bank Ltd.': auSmallFinanceBank,
+	'Federal Bank Ltd.': federalBank,
+	'HDFC Bank Ltd': hdfcBank,
+	'ICICI Bank Ltd.': iciciBank,
+	'IDFC FIRST Bank Limited': idfcFirstBank,
+	'Kotak Mahindra Bank Ltd': kotakMahindraBank,
 	'Punjab National Bank': punjabNationalBank,
-	'State Bank of India (SBI)': sbiBank,
+	'State Bank of India': sbiBank,
 	'UCO Bank': ucoBank,
-	'INDIAN Bank': indianBank,
+	'Indian Bank': indianBank,
+
+	'AB Bank Ltd.': abBank,
+	'Airtel Payments Bank Ltd': airtelPaymentsBank,
+	'American Express Banking Corporation': amexCorporation,
+	'Andhra Pradesh Grameena Bank': apgBank,
+	'Arunachal Pradesh Rural Bank': aprBank,
+	'Assam Gramin Vikas Bank': agvBank,
+	'Australia and New Zealand Banking Group Ltd.': anzBank,
+	'Bank of America': boa,
+	'Bank of Bahrain and Kuwait B.S.C.': bankOfBahrainKuwait,
+	'Bank of Baroda': bankOfBaroda,
+	'Bank of Ceylon': bankOfCeylon,
+	'Bank of China Limited': bankOfChina,
+	'Bank of India': bankOfIndia,
+	'Bank of Maharashtra': bankOfMaharashtra,
+	'Bank of Nova Scotia': bankOfNovaScotia,
+	'Bandhan Bank Ltd.': bandhanBank,
+	'Barclays Bank Plc.': barclays,
+	'Bihar Gramin Bank': biharGraminBank,
+	'BNP Paribas': bnpParibas,
+	'Canara Bank': defaultBank,
+	'Capital Small Finance Bank Ltd': defaultBank,
+	'Central Bank of India': defaultBank,
+	'Chhattisgarh Gramin Bank': defaultBank,
+	'Citibank N.A.': defaultBank,
+	'City Union Bank Ltd.': defaultBank,
+	'Coastal Local Area Bank Ltd': defaultBank,
+	'Cooperatieve Rabobank U.A./ Coöperatieve Centrale Raiffeisen-Boerenleenbank B.A.':
+		defaultBank,
+	'Credit Agricole Corporate and Investment Bank': defaultBank,
+	'CSB Bank Limited': defaultBank,
+	'CTBC Bank Co., Ltd.': defaultBank,
+	'DBS Bank India Limited': defaultBank,
+	'DCB Bank Ltd.': defaultBank,
+	'Deutsche Bank A.G.': defaultBank,
+	'Dhanlaxmi Bank Ltd.': defaultBank,
+	'Doha Bank Q.P.S.C': defaultBank,
+	'Emirates NBD Bank P.J.S.C': defaultBank,
+	'Equitas Small Finance Bank Ltd': defaultBank,
+	'ESAF Small Finance Bank Ltd.': defaultBank,
+	'Export-Import Bank of India': defaultBank,
+	'FINO Payments Bank Ltd': defaultBank,
+	'First Abu Dhabi Bank PJSC': defaultBank,
+	'FirstRand Bank Limited': defaultBank,
+	'Gujarat Gramin Bank': defaultBank,
+	'Haryana Gramin Bank': defaultBank,
+	'Himachal Pradesh Gramin Bank': defaultBank,
+	'Hong Kong and Shanghai Banking Corporation Limited': defaultBank,
+	'IDBI Bank Limited': defaultBank,
+	'India Post Payments Bank Ltd': defaultBank,
+	'Indian Overseas Bank': defaultBank,
+	'Industrial and Commercial Bank of China': defaultBank,
+	'Industrial Bank of Korea': defaultBank,
+	'IndusInd Bank Ltd': defaultBank,
+	'Jammu & Kashmir Bank Ltd.': defaultBank,
+	'Jammu and Kashmir Grameen Bank': defaultBank,
+	'Jana Small Finance Bank Ltd': defaultBank,
+	'Jharkhand Gramin Bank': defaultBank,
+	'Jio Payments Bank Ltd': defaultBank,
+	'J.P. Morgan Chase Bank N.A.': defaultBank,
+	'JSC VTB Bank': defaultBank,
+	'Karnataka Bank Ltd.': defaultBank,
+	'Karnataka Grameena Bank': defaultBank,
+	'Karur Vysya Bank Ltd.': defaultBank,
+	'KEB Hana Bank': defaultBank,
+	'Kerala Grameena Bank': defaultBank,
+	'Krishna Bhima Samruddhi LAB Ltd': defaultBank,
+	'Kookmin Bank': defaultBank,
+	'Madhya Pradesh Gramin Bank': defaultBank,
+	'Maharashtra Gramin Bank': defaultBank,
+	'Manipur Rural Bank': defaultBank,
+	'Mashreq bank P.S.C': defaultBank,
+	'Meghalaya Rural Bank': defaultBank,
+	'Mizoram Rural Bank': defaultBank,
+	'Mizuho Bank Ltd.': defaultBank,
+	'MUFG Bank, Ltd.': defaultBank,
+	'Nagaland Rural Bank': defaultBank,
+	'Nainital bank Ltd.': defaultBank,
+	'NatWest Markets Plc': defaultBank,
+	'National Bank for Agriculture and Rural Development': defaultBank,
+	'National Housing Bank': defaultBank,
+	'Nong Hyup Bank': defaultBank,
+	'NSDL Payments Bank Limited': defaultBank,
+	'Odisha Grameen Bank': defaultBank,
+	'Paytm Payments Bank Ltd': defaultBank,
+	'PT Bank Maybank Indonesia TBK': defaultBank,
+	'Puducherry Grama Bank': defaultBank,
+	'Punjab & Sind Bank': defaultBank,
+	'Punjab Gramin Bank': defaultBank,
+	'Qatar National Bank (Q.P.S.C.)': defaultBank,
+	'Rajasthan Gramin Bank': defaultBank,
+	'RBL Bank Ltd.': defaultBank,
+	'SBM Bank (India) Limited': defaultBank,
+	Sberbank: defaultBank,
+	'Shivalik Small Finance Bank Ltd': defaultBank,
+	'Shinhan Bank': defaultBank,
+	'slice Small Finance Bank Ltd.': defaultBank,
+	'Small Industries Development Bank of India': defaultBank,
+	'Societe Generale': defaultBank,
+	'Sonali Bank Ltd.': defaultBank,
+	'South Indian Bank Ltd.': defaultBank,
+	'Standard Chartered Bank': defaultBank,
+	'Sumitomo Mitsui Banking Corporation': defaultBank,
+	'Suryoday Small Finance Bank Ltd.': defaultBank,
+	'Tamil Nadu Grama Bank': defaultBank,
+	'Tamilnad Mercantile Bank Ltd.': defaultBank,
+	'Telangana Grameena Bank': defaultBank,
+	'Tripura Gramin Bank': defaultBank,
+	'United Overseas Bank Limited': defaultBank,
+	'Unity Small Finance Bank Ltd': defaultBank,
+	'Uttar Pradesh Gramin Bank': defaultBank,
+	'Uttarakhand Gramin Bank': defaultBank,
+	'UBS AG': defaultBank,
+	'Utkarsh Small Finance Bank Ltd.': defaultBank,
+	'West Bengal Gramin Bank': defaultBank,
+	'Woori Bank': defaultBank,
+	'YES Bank Ltd.': defaultBank,
 	Default: defaultBank,
 };
 
